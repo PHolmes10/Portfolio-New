ad80176aa270bdd552bb9d9ba27477342a65b969
pholmes10 <pholmes10@gmail.com>
2019-02-05 12:19:56 -0600

    added grid to project cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NavbarOne from './Components/NavbarOne';
 import JumbotronOne from './Components/JumbotronOne';
+import CardOne from './Components/CardOne'
+import './index.css';
 
 class App extends Component {
   render() {
@@ -9,6 +11,15 @@ class App extends Component {
         <header className="App-header">
           <NavbarOne></NavbarOne>
           <JumbotronOne></JumbotronOne>
+          <h3>Projects</h3>
+          <div className='grid'>
+          <CardOne className='grid-item'></CardOne>
+          <CardOne></CardOne>
+          <CardOne></CardOne>
+          <CardOne></CardOne>
+          <CardOne></CardOne>
+          <CardOne></CardOne>
+          </div>
         </header>
       </div>
     );
--- client/src/Components/NavbarOne.js
+++ client/src/Components/NavbarOne.js
@@ -30,7 +30,7 @@ export default class NavbarOne extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">Patrick Holmes</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
